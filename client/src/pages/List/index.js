8c463cb1cbd03cbe1219f7a773a3577f209ec153
Louis <louisapstg@gmail.com>
2022-10-29 16:18:46 +0700

    Add ListPages

---
+++ client/src/pages/List/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  Navbar,
+  BookList,
+  Footer
+} from '../../components'
+
+const List = () => {
+  return (
+    <div>
+      <Navbar />
+      <BookList />
+      <Footer />
+    </div>
+  )
+}
+
+export default List
