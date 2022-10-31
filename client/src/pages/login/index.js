01b359c3c5b97595da8dd12768fa2eed7e86dcbd
Louis <louisapstg@gmail.com>
2022-10-31 09:26:57 +0700

    Add Login Pages

---
+++ client/src/pages/login/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+  Navbar,
+  Footer
+} from '../../components'
+
+const Login = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default Login
