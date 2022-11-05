e2a6a8625d652a11a222d16e838d64efd68d3c5b
Louis <louisapstg@gmail.com>
2022-11-05 10:51:53 +0700

    Add table to edit data

--- client/src/components/formEditBook/index.js
+++ client/src/components/formEditBook/index.js
@@ -1,243 +1,54 @@
 import React from 'react'
 import {
-  MDBRow,
-  MDBCol,
-  MDBCard,
-  MDBCardBody,
-  MDBCardTitle,
-  MDBCardText,
-  MDBBtn
+  MDBBtn,
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody
 } from 'mdb-react-ui-kit'
 
 const FormEditBook = () => {
   return (
-    <div className="container">
-      <MDBRow className='row-cols-1 row-cols-md-3 g-4 mt-4 p-5'>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
+    <div className="container-fluid mt-5 pt-5 mb-8">
+      <MDBTable align='middle'>
+        <MDBTableHead>
+          <tr className='text-center'>
+            <th scope='col' className='bg-danger'>Book</th>
+            <th scope='col' className='bg-warning'>Author</th>
+            <th scope='col' className='bg-danger th-w'>Description</th>
+            <th scope='col' className='bg-warning th-w2'>Actions</th>
+          </tr>
+        </MDBTableHead>
+        <MDBTableBody>
+          <tr>
+            <td>
+              <div className='d-flex align-items-center'>
+                <img
+                  src='https://mdbootstrap.com/img/new/avatars/8.jpg'
+                  alt=''
+                  style={{ width: '45px', height: '45px' }}
+                  className='rounded-circle'
+                />
+                <div className='ms-3'>
+                  <p className='fw-bold mb-1'>John Doe</p>
+                  <p className='text-muted mb-0'>john.doe@gmail.com</p>
+                </div>
+              </div>
+            </td>
+            <td>
+              <p className='fw-normal mb-1'>Software engineer</p>
+            </td>
+            <td>Senior</td>
+            <td className='text-center'>
+              <MDBBtn className='me-4' color='warning' rounded size='sm'>
                 Edit
               </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
+              <MDBBtn color='danger' rounded size='sm'>
                 Delete
               </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-        <MDBCol className='w-25'>
-          <MDBCard>
-            <MDBCardBody className='light-blue'>
-              <MDBCardTitle className='fw-bold'>Harry Potter and the Philosopher's Stone</MDBCardTitle>
-              <MDBCardText>
-                Harry Potter and the Philosopher's Stone merupakan sebuah novel fantasi pada tahun 1997 yang ditulis oleh J. K. Rowling
-              </MDBCardText>
-              <MDBBtn className='float-start' color='warning' rounded>
-                Edit
-              </MDBBtn>
-              <MDBBtn className='float-end' color='danger' rounded>
-                Delete
-              </MDBBtn>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
+            </td>
+          </tr>
+        </MDBTableBody>
+      </MDBTable>
     </div>
   )
 }
